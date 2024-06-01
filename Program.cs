f391a074437107d2004eaa22eb84055ef47454a6
li-guohao <git@liguohao.cn>
2024-06-02 00:17:21 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        /*
+        * R e a d m e
+        * -----------
+        * Production equipment graphical display for LCD by Space Engineers Script.
+        * 太空工程师，生产设备图形化展示脚本。
+        * 
+        * @see <https://github.com/se-scripts/produce-graphic>
+        * @author [Hi.James](https://space.bilibili.com/368005035)
+        * @author [li-guohao](https://github.com/li-guohao)
+        */
+
+        public Program()
+        {
+
+        }
+
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+
+        }
+    }
+}
